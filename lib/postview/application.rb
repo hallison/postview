7cf6d9f03183c2535d4c6db412597e27e7af3846
Hallison Batista <email@hallisonbatista.com>
2009-10-23 12:12:47 -0400

    Included deprecation warning message
    * Deprecation warning message has been included in Postview::Application
      module. The blog application is Postview::Application::Blog.

--- lib/postview/application.rb
+++ lib/postview/application.rb
@@ -5,6 +5,20 @@ module Application
 
   autoload :Blog, 'postview/application/blog'
 
+  def self.call(env)
+    puts <<-end_msg.gsub(/^[ ]{6}/,'')
+      #{Postview::version_summary}
+      >> DEPRECATION WARNING
+         The run Postview::Application is deprecated. Instead use
+         Postview::Application::Blog to call blogware.
+
+         Thanks.
+         
+         Visit http://github.com/hallison/postview/tree/news for more information.
+    end_msg
+    Blog.call(env)
+  end
+
 end # module Application
 
 end # module Postview
